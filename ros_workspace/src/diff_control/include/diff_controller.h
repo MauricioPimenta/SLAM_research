ef44d2eac26eb5d96dc855e4c3a74f092e385d8b
MauricioPimenta <mauriciobpimenta@gmail.com>
2025-01-24 21:13:32 -0300

    created the launcher file, modified CMakeLists for ROS, added dependencies on package.xml and added a subscriber to a 'goal' topic for testing.

--- ros_workspace/src/diff_control/include/diff_controller.h
+++ ros_workspace/src/diff_control/include/diff_controller.h
@@ -8,7 +8,7 @@
  *
  *
  ** Subscribes to:
- *      * vrpn twist topic from the vrpn_client_ros package (name defined by the launch file)
+ *      * vrpn Pose topic from the vrpn_client_ros package (name defined by the launch file)
  *      * pose topic from the slam_toolbox: pose of the base_frame along with the covariance
  **       calculated from the scan match
  *
@@ -26,17 +26,61 @@
 /* ROS */
 #include <ros/ros.h>
 
+
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Pose.h>
 
 /* tf is needed to convert quaternions to Euler and vice versa */
-#include <tf.h>
+#include <tf/tf.h>
+#include <tf2/convert.h>
 
 
 class diff_controller
 {
+    private:
+    /*
+     * Private Atributes and Properties
+     */
+    ros::NodeHandle nh_;
+    ros::NodeHandle priv_nh_;
+    ros::Subscriber vrpn_sub_;
+    ros::Subscriber pose_sub_;
+    ros::Subscriber goal_sub_;
+    ros::Publisher cmd_vel_pub_;
+
+    /*
+     * Parameters
+     */
+        std::string vrpn_topic_;
+        std::string pose_topic_;
+        std::string cmd_vel_topic_;
+        std::string goal_topic_;
+
+        // Controller Parameters
+        double a_;
+        double Kx_;
+        double Ky_;
+
+
+    geometry_msgs::PoseStamped vrpn_twist_;
+    geometry_msgs::PoseWithCovarianceStamped slam_pose_;
+    geometry_msgs::Twist cmd_vel_;
+    geometry_msgs::Pose goal_;
+
+    // Arrays and Lists
+    std::vector<geometry_msgs::PoseStamped> vrpn_twist_list_;
+    std::vector<geometry_msgs::PoseWithCovarianceStamped> slam_pose_list_;
+    std::vector<geometry_msgs::Twist> cmd_vel_list_;
+    std::vector<geometry_msgs::Pose> goal_list_;
+
+    /*
+     * Private Methods
+     */
+
+
+
 public:
     /*
      * Public Atributes and Properties
@@ -47,12 +91,13 @@ public:
      */
 
     // Constructor
-    diff_controller() : nh_("controller"), priv_nh_("~")
+    diff_controller() : nh_(""), priv_nh_("~")
     {
-        // Get and Set the Parameters from launch file
-        priv_nh_.param<std::string>("vrpn_topic", vrpn_topic_, "vrpn_client_node/twist");
-        priv_nh_.param<std::string>("pose_topic", pose_topic_, "slam_toolbox/pose");
-        priv_nh_.param<std::string>("cmd_vel_topic", cmd_vel_topic_, "cmd_vel");
+        // Get the Parameters from the launch file and set default values
+        priv_nh_.param<std::string>("vrpn_topic", vrpn_topic_, "/vrpn_client_node/L1/pose");
+        priv_nh_.param<std::string>("pose_topic", pose_topic_, "/slam_toolbox/pose");
+        priv_nh_.param<std::string>("cmd_vel_topic", cmd_vel_topic_, "/cmd_vel");
+        priv_nh_.param<std::string>("goal_topic", goal_topic_, "/goal");
         priv_nh_.param<double>("a", a_, 0.1);
         priv_nh_.param<double>("Kx", Kx_, 1.0);
         priv_nh_.param<double>("Ky", Ky_, 1.0);
@@ -66,6 +111,8 @@ public:
         vrpn_sub_ = nh_.subscribe(vrpn_topic_, 1, &diff_controller::vrpnCallback, this);
         // subscribe to the pose topic from the slam_toolbox
         pose_sub_ = nh_.subscribe(pose_topic_, 1, &diff_controller::slam_poseCallback, this);
+        // subscribe to the goal topic
+        goal_sub_ = nh_.subscribe(goal_topic_, 1, &diff_controller::goalCallback, this);
 
         /*
         * Publishers
@@ -81,17 +128,18 @@ public:
     *  Gets information of the robot position from the optitrack system.
     *  Used as reference to compare with the results from the slam_toolbox.
      ****************************************************************************/
-    void vrpnCallback(const geometry_msgs::Twist::ConstPtr& msg)
+    void vrpnCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
     {
-        ROS_INFO("\nGetting Twist Message from vrpn_client_ros...");
+        ROS_DEBUG("\nGetting Twist Message from %s...", vrpn_topic_.c_str());
 
         // save the message
-        vrpn_twist_.linear.x = msg->linear.x;
-        vrpn_twist_.linear.y = msg->linear.y;
-        vrpn_twist_.linear.z = msg->linear.z;
-        vrpn_twist_.angular.x = msg->angular.x;
-        vrpn_twist_.angular.y = msg->angular.y;
-        vrpn_twist_.angular.z = msg->angular.z;
+        vrpn_twist_.pose.position.x = msg->pose.position.x;
+        vrpn_twist_.pose.position.y = msg->pose.position.y;
+        vrpn_twist_.pose.position.z = msg->pose.position.z;
+        vrpn_twist_.pose.orientation.x = msg->pose.orientation.x;
+        vrpn_twist_.pose.orientation.y = msg->pose.orientation.y;
+        vrpn_twist_.pose.orientation.z = msg->pose.orientation.z;
+        vrpn_twist_.pose.orientation.w = msg->pose.orientation.w;
 
         // add the message to an array of all messages
         vrpn_twist_list_.push_back(vrpn_twist_);
@@ -107,7 +155,7 @@ public:
     */
     void slam_poseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg)
     {
-        ROS_INFO("\nGetting Pose Message from slam_toolbox...");
+        ROS_DEBUG("\nGetting Pose Message from %s...", pose_topic_.c_str());
 
         // save the message
         slam_pose_.header.seq = msg->header.seq;
@@ -128,6 +176,32 @@ public:
 
         // calculate the velocity commands
         calculateVelocities();
+        this->calculateControlSignals(slam_pose_.pose.pose, goal_);
+
+    }
+
+    /****************************************************************************************
+     * Goal Message Callback
+     * @param msg: goal message
+     * 
+     * This function is called whenever a new goal message is received.
+     * Gets the desired position of the robot.
+     */
+    void goalCallback(const geometry_msgs::Pose::ConstPtr& msg)
+    {
+        ROS_DEBUG("\nGetting Goal Message from %s...", goal_topic_.c_str());
+
+        // save the message
+        goal_.position.x = msg->position.x;
+        goal_.position.y = msg->position.y;
+        goal_.position.z = msg->position.z;
+        goal_.orientation.x = msg->orientation.x;
+        goal_.orientation.y = msg->orientation.y;
+        goal_.orientation.z = msg->orientation.z;
+        goal_.orientation.w = msg->orientation.w;
+
+        // add the message to an array of all messages
+        goal_list_.push_back(goal_);
 
     }
 
@@ -164,6 +238,16 @@ public:
         double x_error = x_d - x;
         double y_error = y_d - y;
 
+        // Se a posicao desejada for alcancada
+        if (x_error < 0.01 && y_error < 0.01)
+        {
+            // zera os comandos de velocidade
+            cmd_vel_.linear.x = 0.0;
+            cmd_vel_.angular.z = 0.0;
+            cmd_vel_pub_.publish(cmd_vel_);
+            return;
+        }
+
         // Velocidades desejadas
         double Vxd = 1.0;
         double Vyd = 1.0;
@@ -180,40 +264,5 @@ public:
 
     }
 
-private:
-    /*
-     * Private Atributes and Properties
-     */
-    ros::NodeHandle nh_;
-    ros::NodeHandle priv_nh_;
-    ros::Subscriber vrpn_sub_;
-    ros::Subscriber pose_sub_;
-    ros::Publisher cmd_vel_pub_;
-
-    /*
-     * Parameters
-     */
-        std::string vrpn_topic_;
-        std::string pose_topic_;
-        std::string cmd_vel_topic_;
-
-        // Controller Parameters
-        double a_;
-        double Kx_;
-        double Ky_;
-
-
-    geometry_msgs::Twist vrpn_twist_;
-    geometry_msgs::PoseWithCovarianceStamped slam_pose_;
-    geometry_msgs::Twist cmd_vel_;
-
-    // Arrays and Lists
-    std::vector<geometry_msgs::Twist> vrpn_twist_list_;
-    std::vector<geometry_msgs::PoseWithCovarianceStamped> slam_pose_list_;
-    std::vector<geometry_msgs::Twist> cmd_vel_list_;
-
-    /*
-     * Private Methods
-     */
 
 };
